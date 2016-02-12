9c85cdb50e402ca8ff3b32020dd6cebcc9a0e3a4
m2tiil <shenga.ret@gmail.com>
2016-02-12 16:59:40 +0100

    ajout models animation et option

---
+++ models/Animation.js
@@ -0,0 +1,14 @@
+/**
+ * Created by m2tiil on 2/12/16.
+ */
+var mongoose = require('mongoose');
+
+
+
+var AnimationSchema = new mongoose.Schema({
+
+});
+
+
+
+mongoose.model('Animation', AnimationSchema);
